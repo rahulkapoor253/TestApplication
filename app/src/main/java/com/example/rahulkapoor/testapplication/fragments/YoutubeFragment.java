5c33eafd8a365e474a2163aabf1bb9527b6764bf
Rahul Kapoor <rahul253801@gmail.com>
2018-08-01 13:51:35 +0530

    basic youtube player implemented

--- app/src/main/java/com/example/rahulkapoor/testapplication/fragments/YoutubeFragment.java
+++ app/src/main/java/com/example/rahulkapoor/testapplication/fragments/YoutubeFragment.java
@@ -1,24 +1,47 @@
 package com.example.rahulkapoor.testapplication.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.rahulkapoor.testapplication.HomeActivity;
 import com.example.rahulkapoor.testapplication.R;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class YoutubeFragment extends Fragment {
+public class YoutubeFragment extends YouTubePlayerSupportFragment implements YouTubePlayer.OnInitializedListener {
+
+    private YouTubePlayerView playerView;
+    private static final int RECOVERY_REQUEST = 1;
+    private static final String videoPlaylist = "PLonJJ3BVjZW6CtAMbJz1XD8ELUs1KXaTD";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_youtube, container, false);
 
+        playerView = (YouTubePlayerView) v.findViewById(R.id.youtubeplayer_view);
+        playerView.initialize(getResources().getString(R.string.youtube_api_key), this);
+
 
         return v;
+    }
+
+    @Override
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RECOVERY_REQUEST) {
+            //retry initialising the player view;
+            playerView.initialize(getResources().getString(R.string.youtube_api_key), this);
+        }
+
     }
 
 
@@ -27,5 +50,20 @@ public class YoutubeFragment extends Fragment {
         YoutubeFragment frag = new YoutubeFragment();
 
         return frag;
+    }
+
+    @Override
+    public void onInitializationSuccess(final YouTubePlayer.Provider provider, final YouTubePlayer youTubePlayer, final boolean restored) {
+
+        if (!restored) {
+            youTubePlayer.cueVideo(videoPlaylist);
+        }
+
+    }
+
+    @Override
+    public void onInitializationFailure(final YouTubePlayer.Provider provider, final YouTubeInitializationResult youTubeInitializationResult) {
+        youTubeInitializationResult.getErrorDialog(((HomeActivity) getActivity()), RECOVERY_REQUEST).show();
+
     }
 }
